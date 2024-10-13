eabb4fafc878b1cc09415ad641905bc16a91714a
Oscar Pavon <pavon_oscar@outlook.com>
2024-10-13 00:56:02 +0000

    USB boot

--- config.h
+++ config.h
@@ -5,10 +5,12 @@ bool show_menu = true;
 
 static const BootLoaderEntry entries[] = {
 	{u"Pavon VM", u"vmlinuz", u"initcall_debug quiet root=/dev/sda1 rw fstype=vfat init=/sbin/init"},
-	{u"Pavon stinit quiet initcall debug", u"vmlinuz", u"initcall_debug quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/pinit"},
+	{u"Pavon USB", u"vmlinuz",
+		u"rootwait root=PARTUUID=45e8184f-fd5c-4e77-a94e-b16a4a9823cb rw fstype=ext4"},
+	{u"Pavon stinit quiet initcall debug", u"pavonlinuz", u"initcall_debug quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/pinit"},
 	{u"Pavon sinit usb off", u"vmlinuz", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/pinit usbcore.authorized_default=0"},
 	{u"Pavon /sbin/pinit", u"vmlinuz", u"root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/pinit"},
 	{u"Pavon pinit quiet", u"vmlinuz", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4"},
-	{u"Pavon sinit", u"vmlinuz", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/sinit"},
+	{u"Pavon sinit", u"pavonlinuz", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/sinit"},
 	{u"Gentoo", u"vmlinuz", u"quiet root=/dev/nvme0n1p2 ro fstype=ext4"},
 };
