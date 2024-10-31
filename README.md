e5f0becb025857691a34b17e81ec522bcc0db016
Oscar Pavon <pavon_oscar@outlook.com>
2024-10-31 01:12:05 +0000

    README updated with bootloader.img

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-This will be a suckless bootloader
+![bootloader](bootloader.png)
+
+pboot is an ultra minimalistic EFI Linux bootloader developed with the suckless philosophy
+
+100% functional in real environments.
+
+You can load Linux kernels with parameters.
+
+It's configure with config.h file and configuration take effects after compilation
+like suckless's projects
+
+How to use:
+If the configuration "show_menu" is "true" in config.h the bootloader will show
+the menu infinitlly unless you press the rigth arrow key -> to boot the entry
+You can navigate with the arrow keys up and down.
+
+If the configuration "show_menu" is "false" in config.h the bootloader
+will boot the "default_entry" defined with a "int" number in config.h
+You can press the left arrow key <- after press the computer power button
+to show the menu if you want to choose an entry
+
+This bootloader doesn't need any more feature
+Do one thing and do it well
--- config.h
+++ config.h
@@ -1,17 +1,17 @@
 #include "types.h"
 
 uint8_t default_entry = 6;
-bool show_menu = false;
+bool show_menu = true;
 
 static const BootLoaderEntry entries[] = {
+	{u"Pavon Linux", u"pavonlinuz", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/pinit"},
+	{u"Gentoo", u"vmlinuz", u"quiet root=/dev/nvme0n1p2 ro fstype=ext4"},
+	{u"PKernel", u"pkernel", u""},
 	{u"Pavon VM", u"vmlinuz", u"initcall_debug quiet root=/dev/sda1 rw fstype=vfat init=/sbin/init"},
-	{u"Pavon USB", u"vmlinuz",
+	{u"Pavon Linux USB", u"vmlinuz",
 		u"rootwait root=PARTUUID=45e8184f-fd5c-4e77-a94e-b16a4a9823cb rw fstype=ext4"},
 	{u"Pavon stinit quiet initcall debug", u"pavonlinuz", u"initcall_debug quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/pinit"},
 	{u"Pavon sinit usb off", u"vmlinuz", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/pinit usbcore.authorized_default=0"},
 	{u"Pavon /sbin/pinit", u"vmlinuz", u"root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/pinit"},
-	{u"Pavon pinit quiet", u"vmlinuz", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4"},
-	{u"PKernel", u"pkernel", u""},
 	{u"Pavon sinit", u"pavonlinuz", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/sbin/sinit"},
-	{u"Gentoo", u"vmlinuz", u"quiet root=/dev/nvme0n1p2 ro fstype=ext4"},
 };
