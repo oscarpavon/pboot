2fe9ac596d2e0b2f4cb369c9cc4d17ab19433e3b
Oscar Pavon <pavon_oscar@outlook.com>
2025-03-05 16:31:17 +0000

    added Ubuntu

--- config.h
+++ config.h
@@ -3,8 +3,9 @@
 static uint8_t default_entry = 0;
 static bool show_menu = true;
 
-static BootLoaderEntry entries[4] = {
+static BootLoaderEntry entries[6] = {
 	{u"Pavon Linux", u"pLinux", u"quiet root=/dev/nvme0n1p3 rw fstype=ext4 init=/pinit"},
-	{u"Gentoo", u"pLinux", u"quiet root=/dev/nvme0n1p2 ro fstype=ext4"},
+	{u"Ubuntu", u"ubuntu-linux", u"root=/dev/nvme0n1p2 ro quiet fstype=ext4 splash vt.handoff=7 initrd=ubuntu-initrd"},
 	{u"PKernel", u"pkernel", u""},
+	{u"Pavon VM", u"pavonlinuz", u"quiet root=/dev/sda2 rw fstype=ext4 init=/sbin/pinit"},
 };
